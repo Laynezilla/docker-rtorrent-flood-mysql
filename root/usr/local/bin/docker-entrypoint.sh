00f34695095b35ba88c96e2de3f046f51364d596
Laynezilla <Laynezilla@users.noreply.github.com>
2019-04-10 00:57:09 -0400

    Update docker-entrypoint.sh

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -11,6 +11,6 @@ if [ ! `id -g $PUSER` == $PGID ]; then
 	groupmod -g $PGID $PGROUP
 fi
 
-chown $PUSER:$PGROUP /config /data/{film,games,music,rtorrent/{.session/downloads/watch}} /log /scripts /etc/rtorrent.conf
+chown $PUSER:$PGROUP /config /data/film /data/games /data/music /data/rtorrent/.session /data/rtorrent/downloads /data/rtorrent/watch /log /scripts /etc/rtorrent.conf
 
 exec su-exec $PUSER:$PGROUP "$@"
