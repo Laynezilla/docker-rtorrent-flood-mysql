32a3c07792c4d2d5f9af098194917dac538932bd
Laynezilla <Laynezilla@users.noreply.github.com>
2019-04-09 21:11:17 -0400

    Update docker-entrypoint.sh

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # /usr/local/bin/docker-entrypoint.sh
 
-if [ ! "id -u $PUSER" = $PUID ]; then
+if [ ! `id -u $PUSER` == $PUID ]; then
 	usermod -u $PUID $PUSER
 fi
 
-if [ ! "id -g $PUSER" = $PGID ]; then
+if [ ! `id -g $PUSER` == $PGID ]; then
 	groupmod -g $PGID $PGROUP
 fi
 
 chown $PUSER:$PGROUP /config /data/{film,games,music,rtorrent/{.session/downloads/watch}} /log /scripts /etc/rtorrent.conf
 
-su-exec $PUSER:$PGROUP "$@"
+exec su-exec $PUSER:$PGROUP "$@"
